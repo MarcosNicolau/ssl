ded2626efeab0f572d94220029fbee6389a979e8
nicolau <marcosnicolau2003@gmail.com>
2023-09-16 13:19:32 -0300

    build(tp-02): fix show_help

--- tps/02-Automata/run.sh
+++ tps/02-Automata/run.sh
@@ -1,8 +1,8 @@
 show_help(){
     echo "Usage: $ ./run.sh <PROGRAM_NUMBER> <ARGUMENT>\n"
     echo "EXAMPLE:"
-    echo "Run the first program with the string 012$90$0x321:"
-    echo "$ ./run.sh 1 012$90$0x321\n"
+    echo "Run the first program with the string 012\$90\$0x321:"
+    echo "$ ./run.sh 1 012\\\$90\\\$0x321\n"
     echo "Or run the second program and solve 3*4+12/6:"
     echo "$ ./run.sh 2 3*4+12/6"
 }
